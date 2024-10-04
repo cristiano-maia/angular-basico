255a36b4be1825eda5d9acb846393c54463e5eeb
Cristiano Maia <98246337+cristiano-maia@users.noreply.github.com>
2024-10-04 13:04:18 -0300

    update

---
+++ angular-basico/src/app/componente08/componente08.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente08',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './componente08.component.html',
+  styleUrl: './componente08.component.css'
+})
+export class Componente08Component {
+
+  // variavel de media
+
+  media:number = 8;
+
+}
